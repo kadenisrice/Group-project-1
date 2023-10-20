5cc1b5091739e9885b31ff01fa60cdab887f4291
Kaden Rice <kadenrice13@gmail.com>
2023-10-20 15:29:50 -0400

    e

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 "use strict";
 
-const cardContainer = document.querySelector(".card-container")
+const arrayOfCards = [{}];
 
-for(let i = 0; i < 12; i++) {
+//SELECTORS AND VARIABLES ---------
+
+const cardContainer = document.querySelector(".card-container");
+
+// FUNCTIONS AND EVENTS ------------
+
+const populateBoard = () => {
+  for (let i = 0; i < 12; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("card");
+
     cardContainer.append(newDiv);
-}
+  }
+};
+populateBoard();
